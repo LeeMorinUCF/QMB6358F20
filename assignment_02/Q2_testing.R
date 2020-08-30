ab20eba727aedc6c9ef178d3e7a05e4490646b87
Lee Morin <Lealand.Morin@ucf.edu>
2020-08-30 16:06:26 -0400

    Added material for Assignment 2

---
+++ assignment_02/Q2_testing.R
@@ -0,0 +1,93 @@
+
+################################################################################
+#
+# QMB 6358: Software Tools for Business Analytics
+# Testing a Library of R Functions
+#
+# Name:
+# College of Business Administration
+# University of Central Florida
+#
+# Date:
+#
+################################################################################
+#
+# This program tests a library of R functions.
+#
+#
+#
+################################################################################
+
+
+
+################################################################################
+# Load Packages
+################################################################################
+
+# Load any packages that are needed for these functions.
+# library(package_name_goes_here)
+
+# You would typically install the package first,
+# instead of within the library.
+# install.packages("package_name_goes_here")
+
+# Set working directory, if calling from another location.
+# wd_path <- '~/path/to/your/repo/assignment_02'
+# setwd(wd_path)
+
+
+# Load your source code for the library of functions.
+source("Q1_functions.R")
+# Be sure that this library is located in the working directory.
+# Otherwise, you can include the path.
+
+
+################################################################################
+# Testing Functions
+################################################################################
+
+
+print("Testing function sum_sq_dev:")
+
+# Check values that are output.
+
+print(sum_sq_dev(x = c(0, 0, 0), mu = 0))
+
+# Add two more examples.
+
+
+# Test output against expected answer.
+
+print(sum_sq_dev(x = c(0, 0, 0), mu = 0) == 0)
+
+# Repeat the test of the same two examples.
+
+
+
+print("Testing function covar_y_x:")
+
+# Check values that are output.
+
+print(covar_y_x(x = c(0, 0, 0), mu = c(0, 0, 0)))
+
+# Add two more examples.
+
+
+# Test output against expected answer.
+
+print(covar_y_x(x = c(0, 0, 0), mu = c(0, 0, 0)) == 0)
+
+# Repeat the test of the same two examples.
+
+
+
+
+print("Q1 library testing complete.")
+
+
+
+################################################################################
+# End
+################################################################################
+
+
