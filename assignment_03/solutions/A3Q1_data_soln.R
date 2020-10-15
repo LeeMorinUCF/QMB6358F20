7d5373f2c20f7d30f69e89216185032e2d24c307
Lee Morin <Lealand.Morin@ucf.edu>
2020-10-15 11:17:43 -0400

    Added solutions to Assignment 3

---
+++ assignment_03/solutions/A3Q1_data_soln.R
@@ -0,0 +1,104 @@
+
+################################################################################
+#
+# QMB 6358: Software Tools for Business Analytics
+# Assignment 3: Data Handling
+#
+# Name:
+# College of Business
+# University of Central Florida
+#
+# Date:
+#
+################################################################################
+#
+# This program reads a collection of small datasets to practice handling data.
+# It is a sample of the script for Assignment 3, Question 1.
+#
+#
+#
+################################################################################
+
+# Clear workspace.
+# The remove function removes everything in the workspace when the list is all.
+rm(list=ls(all=TRUE))
+
+# Load library of functions.
+# source('my_R_code.R')
+
+# Load packages.
+# library(name_of_R_package_goes_here)
+
+
+# Set working directory to the path containing the csv files.
+# wd_path <- "/path/to/your/folder
+wd_path <- "C:/Users/le279259/Documents/Teaching/QMB6358_Fall_2020/Assignments/assignment_03/A3_iris_data/QMB6358-iris"
+setwd(wd_path)
+
+
+# Set paths to other folders.
+# data_path <- sprintf('%s/data', wd_path)
+# Optional: Organize data in a separate folder for larger projects.
+
+
+################################################################################
+# Reading data from csv files.
+################################################################################
+
+
+
+# Loop through the number of files in the dataset.
+# Choose a small number of files to start.
+num_files <- 100
+
+# Initialize with an empty object.
+A3Q1_data <- NULL
+for (file_num in 1:num_files) {
+
+  # Set the filename.
+  file_name <- paste('iris_', file_num, '.txt', sep = "")
+
+  # A3Q1a) Print filename.
+  print(file_name)
+
+  # Read this file to a temporary data frame.
+  A3Q1_data_sample <- read.table(file = file_name)
+
+  # A3Q1b) Print table of counts by species (to test file reading).
+  print(table(A3Q1_data_sample[, 6], useNA = 'ifany'))
+
+  # Append this file to the full dataset.
+  A3Q1_data <- rbind(A3Q1_data, A3Q1_data_sample)
+
+
+}
+
+
+################################################################################
+# Testing dataset from Question 1
+################################################################################
+
+
+print("Contents of Dataset for Question 1:")
+
+# Print some statistics about the full dataset
+# to verify the content.
+print("Dimensions of dataset:")
+print(dim(A3Q1_data))
+
+
+# Add column labels if the number of columns is correct.
+if (!is.null(A3Q1_data) && ncol(A3Q1_data) == 6) {
+  colnames(A3Q1_data) <- c("Sample", "Sepal.Length", "Sepal.Width",
+                           "Petal.Length", "Petal.Width",  "Species")
+}
+print(summary(A3Q1_data))
+
+print(table(A3Q1_data[, 6], useNA = 'ifany'))
+
+
+
+################################################################################
+# End
+################################################################################
+
---
+++ assignment_03/solutions/A3Q2_data_soln.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+################################################################################
+#
+# QMB 6358: Software Tools for Business Analytics
+# Shell Script for Creating Datasets in Assignment 3, Question 2
+#
+# Name:
+# College of Business
+# University of Central Florida
+#
+# Date:
+#
+################################################################################
+#
+# This shell script creates a dataset in UNIX, in two ways.
+# It the runs two R scripts to test the contents of the datasets
+# created in UNIX to compare with the results from R.
+#
+# Note: The top line tells where your bash program is located
+#     and should match the result you get when you
+#     type the command "which bash".
+#     To run this script you have to navigate to this folder in
+#     a terminal window, such as GitBash, and execute
+#     ./my_shell_script.sh
+#     where the name of the .sh file corresponds to the name of this file.
+#
+################################################################################
+
+# Bash commands before running R
+# e.g. making directory for output, etc.
+echo "Running test of Assignment 3..."
+
+
+
+################################################################################
+# Question 2: Assemble the data in UNIX
+################################################################################
+
+
+# Question 2 a) Using a loop
+echo "Running commands for Question 2a)..."
+
+# Move to folder containing iris files.
+cd ../../../../Assignments/assignment_03/A3_iris_data/QMB6358-iris
+
+echo "List of files in iris folder:"
+ls -lh
+
+
+# Loop through the number of files in the dataset.
+# Choose a small number of files to start.
+# NUM_FILES="1 2 3 4 5 6 7"
+NUM_FILES=$(seq 1 100)
+
+# Initialize with an empty file.
+echo "" > A3Q2a_full.txt
+for FILE_NUM in $NUM_FILES
+do
+    # Append the current dataset to the full dataset.
+    echo "Loading file A3_iris_data/QMB6358-iris/iris_$FILE_NUM.txt"
+
+    # Insert a cat command here, reading in the numbered dataset
+    # and appending it to the full dataset A3Q2a_full.csv:
+    cat iris_$FILE_NUM.txt >> A3Q2a_full.txt
+
+done
+
+echo "Completed commands for Question 2a)."
+
+
+
+
+
+# Question 2 b) Using a one-line command.
+echo "Running commands for Question 2b)..."
+
+# Insert a cat command here, reading in all numbered datasets
+# and writing to the full dataset A3Q2b_full.csv.
+# One-line command goes here:
+cat iris_*.txt > A3Q2b_full.txt
+
+echo "Completed command for Question 2b)."
+
+
+
+
+################################################################################
+# Testing Question 1: Assemble the data in R
+################################################################################
+
+
+# Move to folder containing R scripts.
+cd ../../../../GitRepos/QMB6358F20/assignment_03/solutions/
+
+
+# Run the R script and save the output
+echo "Running R script for Question 1..."
+Rscript A3Q1_data_soln.R > A3Q1_results_soln.out
+echo "Completed R script for Question 1."
+
+
+################################################################################
+# Testing Question 2: Assemble the data in UNIX. Test it in R
+################################################################################
+
+# Run the R script and save the output
+echo "Running R script for Question 2..."
+
+# Call your A3Q2_tests.R script here.
+Rscript A3Q2_tests_soln.R > A3Q2_results_soln.out
+
+
+echo "Completed R script for Question 2."
+
+
+
+################################################################################
+
+# Other bash commands after output
+# e.g. zip file and save copy somewhere else
+echo "Completed test of Assignment 3."
+
+
+################################################################################
+# end
+################################################################################
